5ad26b0eab7a0c75bd5f1099fd4ab09df21ea162
erikzon <ericksonoliva@gmail.com>
2023-10-20 21:27:35 -0600

    reseteo de mode no-cors

--- pages/modulo/usuarios/index.js
+++ pages/modulo/usuarios/index.js
@@ -56,9 +56,8 @@ export default function Usuarios({ recordset }) {
         activoinactivo ? "1" : "0"
       }`,
       { method: "DELETE",
-      headers: headersList,
-      mode: 'no-cors'
-    }
+        headers: headersList
+      }
     );
     
     peticion
